3bb9efd96d88a19211ea847036080ce10d9775da
Antonin <antonin.anchisi@orange.com>
2021-09-09 17:49:30 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ module "cce" {
   cluster_flavor = "cce.s1.small"
   vpc_id = var.vpc_id
   network_id = var.network_id  
-  cluster_version = "v1.19.8-r0"
+  cluster_version = "v1.19.10-r0"
 
   node_os = "EulerOS 2.5" 
 
